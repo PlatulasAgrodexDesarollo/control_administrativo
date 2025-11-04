d9af783a268dafa2399df19caf190a5a98cb8f23
DiegoM <villegasmisael15@gmail.com>
2025-11-04 10:45:02 -0600

    actualizacion para create.php de operadores

--- resources/views/operadores/create.blade.php
+++ resources/views/operadores/create.blade.php
@@ -1,41 +1,39 @@
-@extends('layouts.base') 
-
-
+@extends('layouts.base', [
+    'ruta' => $ruta, 
+    'texto_boton' => $texto_boton
+]) 
 
 @section('content')
 
     <div class="container mt-4">
-        <h1>Crear Nuevo Operador</h1>
+        <h1>Registrar Nuevo Operador</h1>
 
-        
+        @if ($errors->any())
+            <div class="alert alert-danger"><ul>@foreach ($errors->all() as $error)<li>{{ $error }}</li>@endforeach</ul></div>
+        @endif
 
         <form action="{{ route('operadores.store') }}" method="POST">
-            @csrf {{-- ¡Token de seguridad OBLIGATORIO! --}}
-
+            @csrf 
+            
+            {{-- CAMPO NOMBRE --}}
             <div class="mb-3">
                 <label for="nombre" class="form-label">Nombre:</label>
                 <input type="text" name="nombre" class="form-control" required value="{{ old('nombre') }}">
-                @error('nombre') <div class="text-danger">{{ $message }}</div> @enderror
             </div>
 
+            {{-- CAMPO APELLIDO  --}}
             <div class="mb-3">
                 <label for="apellido" class="form-label">Apellido:</label>
                 <input type="text" name="apellido" class="form-control" value="{{ old('apellido') }}">
             </div>
-
-            <div class="mb-3">
-                <label for="identificacion" class="form-label">Puesto:</label>
-                <input type="text" name="identificacion" class="form-control" required value="{{ old('identificacion') }}">
-                @error('identificacion') <div class="text-danger">{{ $message }}</div> @enderror
-            </div>
             
+            {{-- CAMPO PUESTO --}}
             <div class="mb-3">
-                <label for="observaciones" class="form-label">Observaciones:</label>
-                <textarea name="observaciones" class="form-control">{{ old('observaciones') }}</textarea>
+                <label for="puesto" class="form-label">Puesto:</label>
+                <input type="text" name="puesto" class="form-control" required value="{{ old('puesto') }}">
             </div>
 
             <button type="submit" class="btn btn-success">Guardar Operador</button>
         </form>
     </div>
-
-@endsection 
+@endsection
