af57e8f391e6ab28974dc995bde9badddcd11bd1
DiegoM <villegasmisael15@gmail.com>
2025-11-12 13:56:34 -0600

    actualizacion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\OperadorController;
 use App\Http\Controllers\VariedadController;
 use App\Http\Controllers\LlegadaPlantaController;
 use App\Http\Controllers\PlantacionController;
+use App\Http\Controllers\ControlPlagasController;
+use App\Http\Controllers\AclimatacionController;
+use App\Http\Controllers\ChequeoHyTController;
+use App\Http\Controllers\ChequeoAgribonController;
+use App\Http\Controllers\EndurecimientoController;
 
 
 Route::get('/login', function () {
@@ -43,16 +48,30 @@ Route::get('/plantacion', function () {
     return "Cargando Módulo Plantación...";
 })->name('plantacion.index');
 
+//control plagas:
+Route::get('/control_plagas', function(){
+    return "cargando modulo Control plagas...";
+})->name('control_plagas.index');
+
 
 
 
 Route::get('operadores/listaoperadores', [OperadorController::class, 'listaoperadores'])->name('operadores.listaoperadores'); 
-
+Route::get('control_plagas/create/{etapa_type}/{etapa_id}', [App\Http\Controllers\ControlPlagasController::class, 'create'])->name('control_plagas.create');
+Route::get('chequeo_hyt/create/{aclimatacion_id}', [ChequeoHyTController::class, 'create'])->name('chequeo_hyt.create');
+Route::get('chequeo_agribon/create/{aclimatacion_id}', [ChequeoAgribonController::class, 'create'])->name('chequeo_agribon.create');
 
 Route::resource('operadores', OperadorController::class)  ->parameters(['operadores' => 'operador']);
 Route::resource('variedades', VariedadController::class) ->parameters(['variedades' => 'variedad']);
 Route::resource('llegada_planta', LlegadaPlantaController::class) ->parameters(['llegada_planta' => 'llegada_planta']);
 Route::resource('plantacion', PlantacionController::class) ->parameters(['plantacion' => 'plantacion']);
+Route::resource('control_plagas', ControlPlagasController::class);
+Route::resource('aclimatacion', AclimatacionController::class) ->parameters(['aclimatacion' => 'aclimatacion']);
+Route::resource('endurecimiento', EndurecimientoController::class);
+Route::resource('chequeo_hyt', ChequeoHyTController::class);
+Route::resource('chequeo_agribon', ChequeoAgribonController::class);
+
+
 
 Route::put('operadores/{operador}/reactivate', [OperadorController::class, 'reactivate'])->name('operadores.reactivate');
 Route::delete('operadores/{operador}/hard-delete', [OperadorController::class, 'hardDelete'])->name('operadores.hardDelete');
