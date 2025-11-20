9680b3d66e4390d3b378c841859951463dd7954c
DiegoM <villegasmisael15@gmail.com>
2025-11-20 12:48:21 -0600

    implementacion de  @yield en el layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -9,10 +8,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     @vite('resources/js/app.js')
+    @yield('custom_scripts')
 </head>
 
 <body>
