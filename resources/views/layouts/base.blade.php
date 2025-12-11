99fdb848ea39495bd3fbcc3060dd9b632fc0d583
DiegoM <villegasmisael15@gmail.com>
2025-12-11 09:49:57 -0600

    scrypt en base.blade del layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -67,6 +67,15 @@
 
     {{-- Script de Bootstrap necesario para el dropdown --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+       
+        document.addEventListener('DOMContentLoaded', function () {
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+                return new bootstrap.Tooltip(tooltipTriggerEl);
+            });
+        });
+    </script>
 </body>
 
 </html>
