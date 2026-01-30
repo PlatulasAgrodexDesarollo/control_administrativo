7c93174fa0fb0451da6ab232d50e3d489add50e1
DiegoM <villegasmisael15@gmail.com>
2026-01-30 08:54:34 -0600

    models user para la conexion con el proyecto principal

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,32 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $connection = 'mysql_principal';
+
+    // El nombre de la tabla en el proyecto principal
+    protected $table = 'operadores';
+
+    // Tu llave primaria personalizada
+    protected $primaryKey = 'ID_Operador';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'Nombre', 'Apellido_P', 'Apellido_M', 'Puesto', 
+        'Usuario', 'Contrasena_Hash', 'current_session_id', 
+        'last_activity', 'Activo', 'ID_Rol'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function getAuthPassword()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->Contrasena_Hash;
     }
+
+    // Desactivamos los timestamps si la tabla no tiene 'created_at' y 'updated_at'
+    public $timestamps = false;
 }
