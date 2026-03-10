2459aca82d656802382ab07bbe47b0d4a48888b6
DiegoM <villegasmisael15@gmail.com>
2026-03-10 09:37:17 -0600

    rutas en la pagina principal para la funcion correcta de los estilos

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'])
     @yield('custom_scripts')
 </head>
 
